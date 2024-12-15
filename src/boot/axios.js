eaf5bb1f03591bd91db888bb4eb80bacc2e686cc
Suh-Edmond <suhedmond25@yahoo.com>
2024-12-15 17:43:51 +0100

    add deployed app url

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -8,7 +8,7 @@ import { LocalStorage } from "quasar";
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: "http://localhost:8000/api/" });
+const api = axios.create({ baseURL: "http://13.60.224.90/api/" });
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
